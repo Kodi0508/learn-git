31835076c9e600ced6cb37c0e1d10566adad35d5
Kodi0508 <kodi0508rk@gmail.com>
2020-11-15 07:21:16 +0400

    Adding Stylesheet (Bootstrap)

---
+++ products.js
@@ -0,0 +1,92 @@
+let products = [
+  {
+    "id": 1010,
+    "title": "Sports Club",
+    "location": "San Francisco",
+    "price": 25.00,
+    "image": "Sports.jpg",
+    "availableInventory": 5,
+    
+  },
+  {
+    "id": 1009,
+    "title": "Book Club",
+    "location": "Miami",
+    "price": 20.00,
+    "image": "Book.jpg",
+    "availableInventory": 5,
+    
+  },
+  {
+    "id": 1008,
+    "title": "History Club",
+    "location": "Philadelphia",
+    "price": 30.00,
+    "image": "History.jpg",
+    "availableInventory": 5,
+    
+  },
+  {
+    "id": 1007,
+    "title": "Geography Club",
+    "location": "Houston",
+    "price": 35.00,
+    "image": "Geography.jpg",
+    "availableInventory": 5,
+    
+  },
+  {
+    "id": 1006,
+    "title": "Cooking Club",
+    "location": "Austin",
+    "price": 40.00,
+    "image": "Cooking.jpg",
+    "availableInventory": 5,
+    
+  },
+  {
+    "id": 1005,
+    "title": "Music Club",
+    "location": "Los Angeles",
+    "price": 32.00,
+    "image": "Music.jpg",
+    "availableInventory": 5,
+    
+  },
+  {
+    "id": 1004,
+    "title": "Spanish Club",
+    "location": "New York",
+    "price": 27.00,
+    "image": "Spanish.png",
+    "availableInventory": 5,
+    
+  },
+  {
+    "id": 1003,
+    "title": "Literature Club",
+    "location": "Phoenix",
+    "price": 23.00,
+    "image": "Literature.jpg",
+    "availableInventory": 5,
+    
+  },
+  {
+    "id": 1002,
+    "title": "Theatre Club",
+    "location": "Las Vegas",
+    "price": 22.00,
+    "image": "Theatre.jpg",
+    "availableInventory": 5,
+    
+  },
+  {
+    "id": 1001,
+    "title": "French Club",
+    "location": "San Diego",
+    "price": 15.00,
+    "image": "French.jpg",
+    "availableInventory": 5,
+    
+  }
+]
